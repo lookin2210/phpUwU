4fe8d45cd8e3c52a9fcfa8b494da4ccb8199bcc4
lookin2210 <36889@bpk.ac.th>
2025-05-30 16:05:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = "นางสาวสาวิตรี มินสะมัน";
+        $b = "ม.6";
+        $c = "9";
+        $d = "19";
+        $e = "มัธยม";
+        echo '<br>ให้นักเรียนเขียนคำสั่ง ชื่อ-สกุล : ($a)';
+        echo '<br>เรียนอยู่ระดับชั้น : ($b)';
+        echo '<br>ห้องเรียน : ($c)';
+        echo '<br>เลขที่ : ($d)';
+        echo '<br>ประวัติการศึกษา : ($e)';
+    ?>
+</body>
+</html>
